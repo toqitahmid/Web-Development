bccb1010bd55d88997c1b8a9865a6d34d62c4b19
toqitahmid <rubaiathali@gmail.com>
2025-10-19 13:53:15 +0600

    learn basic of JS

---
+++ Javascript/BasicJs/Basic.js
@@ -0,0 +1,18 @@
+const myId = 1211;
+let myEmail = "araf@gmail.com";
+var accountPassword = 147972;
+city = "Dinajpur"; 
+
+// myId = 45434   <--  // is not valid
+
+accountPassword = 123;
+city = "Dhaka";
+console.log(city);
+
+/*
+after learning let var 
+we don't use var for it's 
+scop limitetion and bloks
+*/
+
+console.table([myId, myEmail, accountPassword, city]);
