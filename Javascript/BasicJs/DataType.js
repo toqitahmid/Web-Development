6ae2ab51c7f946b189471d2e8d91efc17a564715
toqitahmid <rubaiathali@gmail.com>
2025-10-19 13:53:56 +0600

    learn data types

---
+++ Javascript/BasicJs/DataType.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert(2+2); 
+//we are using node js not browser
+
+console.log(2+2);
+
+let name = "Araf";
+let age = 20;
+let have_a_bike = false;
+
+// name -> String
+// age -> integer
+// have_a_bike -> Boolean
+// null -> standalong
+// undefined -> undefined
+// Symbol -> unique
+
+console.log(typeof have_a_bike);
+console.table([name, age, have_a_bike]);
