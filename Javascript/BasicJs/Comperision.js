d59bcbe1de3fc32522a54e9fccfb7ef1024d9243
toqitahmid <rubaiathali@gmail.com>
2025-10-20 13:42:53 +0600

    comperison oparetor

---
+++ Javascript/BasicJs/Comperision.js
@@ -0,0 +1,22 @@
+
+// console.log(1 == 1);
+// console.log(1 >= 1);
+// console.log(1 == 0);
+// console.log(1 <= 0);
+// console.log(1 != 0);
+
+// console.log("1" == 1);
+// console.log("1" >= 1);
+// console.log("1" != 1);
+
+console.log(null == 0);
+console.log(null != 0);
+console.log(null >= 0);
+console.log(null <= 0);
+
+console.log(undefined == 0);
+console.log(undefined != 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+
+
