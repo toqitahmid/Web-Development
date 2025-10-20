f6e25ab812ab1551e297942e62e4b0d26fbf9e28
toqitahmid <rubaiathali@gmail.com>
2025-10-20 13:44:54 +0600

    stack and heap memory

---
+++ Javascript/BasicJs/Stack&Heap.js
@@ -0,0 +1,16 @@
+let name = "araf";
+let Isname = name;
+Isname = "toqi";
+
+console.log(Isname);
+
+let userOne = {
+    employer : "coder",
+    experience : 2
+}
+
+let usertwo = userOne;
+usertwo.employer = "programmer"
+
+console.log(userOne.employer);
+
