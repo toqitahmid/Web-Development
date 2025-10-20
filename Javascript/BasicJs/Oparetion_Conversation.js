f45802992dc5533f9aaf679408ac67de63c1324f
toqitahmid <rubaiathali@gmail.com>
2025-10-20 13:44:05 +0600

    Run basic oparetion

---
+++ Javascript/BasicJs/Oparetion_Conversation.js
@@ -0,0 +1,35 @@
+
+// let number = 123;
+// console.log(typeof(number));
+
+// let stringNumber = String(number);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+let name = "Araf123";
+console.log(typeof(name));
+console.log(name);
+
+let nameNumber = Number(name);
+console.log(typeof(nameNumber));
+console.log(nameNumber);
+
+// here "Araf123" -> NaN
+
+
+/*  ************ Oparetors ************ */
+
+
+let negative = 10;
+console.log(-negative)
+
+let preIncriminant = 10;
+let pre = ++preIncriminant;
+console.log(pre);
+
+let postIncriminant = 10;
+let post = postIncriminant++;
+console.log(post);
+
+
