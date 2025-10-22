9a0a0e11542023cb70038cdb590f24cbd66e1a50
toqitahmid <rubaiathali@gmail.com>
2025-10-22 21:53:22 +0600

    do some operations

---
+++ Javascript/NumberJs Or MathJs/number.js
@@ -0,0 +1,12 @@
+
+// const value = new Number(121);
+let value = 121;
+console.log(value.toString().length);
+console.log(typeof value);
+
+let num = 245.003614;
+console.log(num.toFixed(1));
+console.log(num.toPrecision(7));
+console.log(num.valueOf())
+console.log(parseInt("-100"))
+console.log(typeof(parseInt("-100")))
