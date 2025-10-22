a569f5fb21bac40d3840819db98a9579f93a23d8
toqitahmid <rubaiathali@gmail.com>
2025-10-22 21:51:58 +0600

    gain knowlege about  strings in js

---
+++ Javascript/Strings/method.js
@@ -0,0 +1,20 @@
+
+const gameName = new String("Free Fire")
+
+console.log(gameName.length);
+console.log(gameName.__proto__);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('i'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const anotherString = "    araf    ";
+console.log(anotherString);
+console.log(anotherString.trim());
+
+const anothernew = "www.abusamsaraf%20portfolio.com"
+console.log(anothernew.replace('%20','/'));
+
+
---
+++ Javascript/Strings/string.js
@@ -0,0 +1,7 @@
+let name = "araf"; 
+let age = 20;
+
+console.log(name +" "+ age);
+console.log("my name is " + name + " and age is " + age);
+console.log(`my name is ${name} and age is ${age}`);
+
