4125ab434d7ad51f94061eafa548c3a16636fff2
toqitahmid <rubaiathali@gmail.com>
2025-10-22 21:50:49 +0600

    learn date in Js

---
+++ Javascript/Date & Time/date.js
@@ -0,0 +1,14 @@
+
+let myDate = new Date();
+// console.log(myDate);
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+console.log(myDate.getDate());
+console.log(myDate.getMonth()+1);
+
+
+let myNewDate = new Date("10-20-2025")
+console.log(myNewDate.toLocaleString());
