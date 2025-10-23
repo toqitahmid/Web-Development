73a35c7ea1ca1c2b1d4ddb71044e53f3de0a9d95
toqitahmid <rubaiathali@gmail.com>
2025-10-23 22:10:15 +0600

    learn Json

---
+++ Javascript/Objects/json.js
@@ -0,0 +1,15 @@
+
+const course = {
+    username : "araf",
+    id : "123",
+    courseInstructor: "Toqi"
+}
+
+const {courseInstructor : instractor} = course;
+console.log(instractor);
+
+// {
+//     "name" : "Araf",
+//     "courseName" : "web dev",
+//     "price" : "free"
+// }
